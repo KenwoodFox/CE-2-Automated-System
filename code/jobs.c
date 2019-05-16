2e541e8f8edb6f7e07bc7a48d9bc7ff73e7ed124
Joe S <kenwood364@gmail.com>
2019-05-15 23:53:37 -0400

    added comments and added the sugarServo

--- code/jobs.c
+++ code/jobs.c
@@ -1,44 +1,52 @@
 task cupDeployJob()
 {
-	cupJobDone = false;
-	delay(700);
-	cupJobDone = true;
-	stopTask(cupDeployJob);
+	cupJobDone = false;	//Set the status of the job to false, the job is not done yet
+	delay(700);	//This delay to simulate the job being done
+
+	cupJobDone = true;	//Set the flag to true, the job is done
+	stopTask(cupDeployJob);	//kermit stop
 }
 
 task sugarDeployJob()
 {
-	sugarJobDone = false;
-	delay(400);
-	sugarJobDone = true;
-	stopTask(sugarDeployJob);
+	sugarJobDone = false;	//Set the status of the job to false, the job is not done yet
+	while(requestedSugar > 0) //For as long as there is sugar requested
+	{
+		servo(sugarServo) = 0;	//Set servo to 0
+		delay(100);	//delay
+		servo(sugarServo) = 180;	//Set servo to 180
+		delay(100);	//delay
+		requestedSugar--;
+	}
+	sugarJobDone = true;	//Set the flag to true, the job is done
+	stopTask(sugarDeployJob);	//kermit stop
 }
 
 task creamerDeployJob()
 {
-	creamerJobDone = false;
-	SensorValue(creamerOut)=true;
-	delay(1000);
-	SensorValue(creamerOut)=false;
-	creamerJobDone = true;
-	stopTask(creamerDeployJob);
+	creamerJobDone = false;	//Set the status of the job to false, the job is not done yet
+	SensorValue(creamerOut)=true;	//Set the creamer to deploy
+	delay(1000);	//delay for a fixed amount of time
+	SensorValue(creamerOut)=false;	//turn off the creamer
+	creamerJobDone = true;	//Set the flag to true, the job is done
+	stopTask(creamerDeployJob);	//kermit stop
 }
 
 task coffeeDeployJob()
 {
-	coffeeJobDone = false;
-	SensorValue(coffeeOut)=true;
-	delay(3000);
-	SensorValue(coffeeOut)=false;
-	coffeeJobDone = true;
-	stopTask(coffeeDeployJob);
+	coffeeJobDone = false;	//Set the status of the job to false, the job is not done yet
+	SensorValue(coffeeOut)=true;	//Set the coffee to deploy
+	delay(3000);	//delay for a fixed amount of time
+	SensorValue(coffeeOut)=false; //turn off the coffee
+	coffeeJobDone = true;	//Set the flag to true, the job is done
+	stopTask(coffeeDeployJob);	//kermit stop
 }
 
 void startAllTasks()
 {
-	startTask(cupDeployJob);
-	startTask(sugarDeployJob);
-	startTask(creamerDeployJob);
-	startTask(coffeeDeployJob);
+	startTask(cupDeployJob);	//Start task
+	startTask(sugarDeployJob);	//Start task
+	startTask(creamerDeployJob);	//Start task
+	startTask(coffeeDeployJob);	//Start task
 	delay(50);
 }
